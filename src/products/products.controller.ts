cfa6774c6783a0994f5fb261b50a54409517a7c0
Marcelo Oliveira <santos.marcelooliveira@gmail.com>
2025-06-04 22:12:16 -0300

    feat: create products controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,6 +1,16 @@
 // src/products/products.controller.ts
-import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Put,
+  HttpException,
+  Delete,
+} from '@nestjs/common';
 import { CreateProductDto } from '../dto/create-product-dto/create-product-dto';
+import { HttpStatus } from '@nestjs/common';
 
 const mockedProducts: CreateProductDto[] = [
   {
@@ -34,7 +44,7 @@ export class ProductsController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return mockedProducts.find((p) => p.id === id);
+    return mockedProducts.find((product) => product.id === id);
   }
 
   @Post()
@@ -46,8 +56,40 @@ export class ProductsController {
       updatedAt: new Date().toISOString(),
       isActive: true,
     };
-    mockedProducts.push(newProduct); // Adiciona ao array mockado
+    mockedProducts.push(newProduct);
     return newProduct;
   }
-  // Implemente PATCH e DELETE de forma similar
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateProductDto: CreateProductDto) {
+    const index = mockedProducts.findIndex((product) => product.id === id);
+    console.log('🚀 ~ ProductsController ~ update ~ index:', index);
+    if (index === -1) {
+      throw new HttpException('Product not found', HttpStatus.NOT_FOUND);
+    }
+    const updatedProduct = {
+      ...mockedProducts[index],
+      ...updateProductDto,
+      updatedAt: new Date().toISOString(),
+    };
+    mockedProducts[index] = updatedProduct;
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Product updated successfully',
+      data: updatedProduct,
+    };
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    const index = mockedProducts.findIndex((product) => product.id === id);
+    if (index === -1) {
+      throw new HttpException('Product not found', HttpStatus.NOT_FOUND);
+    }
+    mockedProducts.splice(index, 1);
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Product deleted successfully',
+    };
+  }
 }
