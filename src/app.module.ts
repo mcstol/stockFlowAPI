8df261d90d36d6dfea3aeb83affa4f57f771bb25
Marcelo Oliveira <santos.marcelooliveira@gmail.com>
2025-06-07 12:18:47 -0300

    feat:  create sales structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { CustomersModule } from './customers/customers.module';
 import { SuppliersModule } from './suppliers/suppliers.module';
+import { SalesModule } from './sales/sales.module';
 
 @Module({
-  imports: [ProductsModule, CustomersModule, SuppliersModule],
+  imports: [ProductsModule, CustomersModule, SuppliersModule, SalesModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/dto/create-sales-dto/create-sales-dto.spec.ts
@@ -0,0 +1,7 @@
+import { CreateSalesDto } from './create-sales-dto';
+
+describe('CreateSalesDto', () => {
+  it('should be defined', () => {
+    expect(new CreateSalesDto()).toBeDefined();
+  });
+});
